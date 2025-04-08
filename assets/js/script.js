6dabbe52ec4ccaf2d74f9ba7fbb1745541f50d26
luan <vanluandvlp@gmail.com>
2025-04-08 21:29:00 +0700

    init project

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/* này là file xử lí logic chung, ko được sửa, nhớ import vào các pages của mình */
+const movies = [];
+const blogs = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector("header");
+  document.addEventListener("scroll", (e) => {
+    console.log(e);
+  })
+})
