bd08e10768c52d341561d9df16838f0006e8af8e
Duy <Nguyendinhduy257@gmail.com>
2025-04-19 14:46:15 +0700

    Sửa header,JS cho slide banner và căn thẳng chiều dọc

---
+++ assets/js/homepage.js
@@ -0,0 +1,16 @@
+    
+const container = document.querySelector(".banner .slider");
+const controlItems = document.querySelectorAll(".control-item");
+
+const WIDTH = document.querySelectorAll(".item")[0].clientWidth;
+console.log(WIDTH)
+controlItems.forEach(item => {
+    item.addEventListener("click", (e) => {
+        console.log(e.target.attributes[0].value);
+        const active = document.querySelector(".active");
+        active.classList.remove("active");
+        e.target.classList.add("active");
+        container.style.transform 
+        = `translate(-${Number(e.target.attributes[0].value)*WIDTH}px)`;
+    })
+})
