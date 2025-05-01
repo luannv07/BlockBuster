853f868ca3c7b2c01d59c628f27a707f15aa977a
luan <vanluandvlp@gmail.com>
2025-05-01 21:09:14 +0700

    responsive header & doi giao dien header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,11 @@
 /* này là file xử lí logic chung, ko được sửa, nhớ import vào các pages của mình */
-const movies = [];
-const blogs = [];
 const minActiveHeader = 400;
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const header = document.querySelector("header");
   const activeNavBtn = document.querySelector(".nav-only-md .list-bar")
 
-  activeNavBtn.addEventListener("click", activeItemCollapse);
 
   document.addEventListener("scroll", () => setTinyHeader(window.scrollY, window.innerWidth, header));
 
@@ -19,13 +17,13 @@ document.addEventListener("DOMContentLoaded", () => {
 const setTinyHeader = (yAxis, wScreen, element) => {
   // console.log(yAxis + " " + wScreen);  
 
-  if (yAxis > minActiveHeader && wScreen > 1240)
+  if (yAxis > minActiveHeader && wScreen >= 992)
     element.classList.add("tiny");
   else
     element.classList.remove("tiny");
 }
-const activeItemCollapse = () => {
-  const containerCollapse = document.querySelector(".container-collapse")
-  containerCollapse.classList.toggle("active-nav-collapse");
 
+const activeNavMd = () => {
+  const containerCollapse = document.querySelector(".container-collapse");
+  containerCollapse.classList.toggle("active-nav-md-menu")
 }
